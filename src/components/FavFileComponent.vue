a98574a6cb3b80d1c90ae2dd3b3bef4dc4a06cbf
Mannequinn <napat.che@student.mahidol.edu>
2023-05-17 03:44:28 +0700

    Edit FavFile

---
+++ src/components/FavFileComponent.vue
@@ -0,0 +1,44 @@
+<script>
+import VuePdfEmbed from 'vue-pdf-embed'
+
+export default {
+    name: 'FavFile',
+    props: ['material'],
+    components: {
+        VuePdfEmbed,
+    },
+    data() {
+        return {
+        }
+    },
+    methods:{
+    },
+}
+</script>
+
+<template>
+    <RouterLink :to="{ path: '/material', name: 'material', params: { file_doc_ref: material[0] } }"  >
+    <div class="col">
+        <div class="card mb-3 h-100 border border-warning">
+            <div class="row">
+                <div class="col-md-6">
+                    <div v-if="material[2] != undefined">
+                        <vue-pdf-embed :source=material[2] :disable-text-layer="true" :page="1" />
+                    </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="card-body">
+                    <div class="row my-4 text-start ms-1">
+                        <h6 class="card-title"><strong>{{ material[1].title }}</strong></h6>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><small><span><b>level: </b>{{ material[1].level }}</span></small></li>
+                        <li class="list-group-item"><small><span><b>Subject: </b>{{ material[1].subject }}</span></small></li>
+                    </ul>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </RouterLink>
+</template>
